8f8e0adc2bda702cfc8ab7203fd8ac6c87eebc08
Andrey Dembskiy-Novikov <dembskiynovikov@gmail.com>
2024-11-28 18:07:36 +0300

    assert add to tests

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,8 @@
+import { strict as assert } from 'node:assert';
 import capitalize from '../src/capitalize.js';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
+assert(capitalize('hello') === 'Hello');
 
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
+assert(capitalize('') === '');
 
 console.log('Все тесты пройдены!');
