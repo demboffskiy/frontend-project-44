350a369c8dc890d487e95bfacc46f1f656093240
Andrey Dembskiy-Novikov <dembskiynovikov@gmail.com>
2024-11-28 15:24:43 +0300

    first tests add

---
+++ tests/capitalize.test.js
@@ -0,0 +1,11 @@
+import capitalize from '../src/capitalize.js';
+
+if (capitalize('hello') !== 'Hello') {
+  throw new Error('Функция работает неверно!');
+}
+
+if (capitalize('') !== '') {
+  throw new Error('Функция работает неверно!');
+}
+
+console.log('Все тесты пройдены!');
